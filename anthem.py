eec0df31e9c6027c7c49235d0a0a5826a3d40dfe
Unero <ramadhanbhg@gmail.com>
2020-12-17 09:59:04 +0700

    Nationalism Update

--- anthem.py
+++ anthem.py
@@ -1,20 +1,11 @@
 import time, pyautogui
 
-def translate(key):
-    """Returns qwerty key or the given key itself if no mapping found"""
-    return "".join(map(lambda x: tr.get(x, x), key))
-
-
-qwerty = """qwertyuiop[]asdfghjkl;'zxcvbnm,.?/QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>"""
-ycuken = """йцукенгшщзхъфывапролджэячсмитьбю,.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"""
-# join as keys and values
-tr = dict(zip(ycuken, qwerty))
-
 if __name__ == "__main__":
     song = open("motherland.txt", 'r', encoding="utf-8")
     time.sleep(5)
     for word in song:
-        pyautogui.typewrite(translate(word))
-        pyautogui.press("enter")
+        pyautogui.typewrite(word)
+        pyautogui.hotkey("shiftleft", "enter")
         time.sleep(2)
+    pyautogui.press("enter")
     song.close()
